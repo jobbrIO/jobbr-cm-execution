9c04236bf09199b24f1b931e3c37c3e542a64e08
Oliver Zürcher <oliver.zuercher@gmail.com>
2017-03-01 11:03:55 +0100

    GetByUniqueId(Guid uniqueId) removed since there is already a unique column

--- source/Jobbr.ComponentModel.Execution/IJobRunInformationService.cs
+++ source/Jobbr.ComponentModel.Execution/IJobRunInformationService.cs
@@ -1,12 +1,9 @@
-﻿using System;
-using Jobbr.ComponentModel.Execution.Model;
+﻿using Jobbr.ComponentModel.Execution.Model;
 
 namespace Jobbr.ComponentModel.Execution
 {
     public interface IJobRunInformationService
     {
-        JobRunInfo GetByUniqueId(Guid uniqueId);
-
         JobRunInfo GetByJobRunId(long jobRunId);
     }
 }

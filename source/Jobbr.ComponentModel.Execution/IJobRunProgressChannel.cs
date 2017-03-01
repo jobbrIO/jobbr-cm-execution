963ef3252d3427dd9bce2936409c308d9a2d6e11
Oliver Zürcher <oliver.zuercher@gmail.com>
2017-03-01 10:50:55 +0100

    appveyor setup

--- source/Jobbr.ComponentModel.Execution/IJobRunProgressChannel.cs
+++ source/Jobbr.ComponentModel.Execution/IJobRunProgressChannel.cs
@@ -1,9 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Jobbr.ComponentModel.Execution.Model;
 
 namespace Jobbr.ComponentModel.Execution
--- tools/build.bat
+++
@@ -1,3 +0,0 @@
-cd ..\source
-"C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\IDE\devenv.exe" Jobbr.ComponentModel.Execution.sln /Build "Release|Any CPU"
-cd ..\tools

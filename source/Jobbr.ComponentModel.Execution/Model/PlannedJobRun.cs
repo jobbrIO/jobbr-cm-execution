fd61dc6f5310e983addb90febdc20f91832d4b14
Oliver Zürcher <oliver.zuercher@gmail.com>
2017-03-01 13:32:14 +0100

    UniqueId Guid -> long

--- source/Jobbr.ComponentModel.Execution/Model/PlannedJobRun.cs
+++ source/Jobbr.ComponentModel.Execution/Model/PlannedJobRun.cs
@@ -4,7 +4,7 @@ namespace Jobbr.ComponentModel.Execution.Model
 {
     public class PlannedJobRun
     {
-        public Guid UniqueId { get; set; }
+        public long UniqueId { get; set; }
 
         public DateTime PlannedStartDateTimeUtc { get; set; }
     }

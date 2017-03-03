7b50360f886528c0b90c20f536af5e79b98124c6
Oliver Zürcher <oliver.zuercher@gmail.com>
2017-03-03 15:15:21 +0100

    interface documented

--- source/Jobbr.ComponentModel.Execution/IJobRunProgressChannel.cs
+++ source/Jobbr.ComponentModel.Execution/IJobRunProgressChannel.cs
@@ -3,6 +3,10 @@ using Jobbr.ComponentModel.Execution.Model;
 
 namespace Jobbr.ComponentModel.Execution
 {
+    /// <summary>
+    /// IJobRunProgressChannel is implemented by the Server. Job Executors report progress
+    /// of jobs via this interface to the server.
+    /// </summary>
     public interface IJobRunProgressChannel
     {
         void PublishStatusUpdate(JobRunInfo jobRunInfo, JobRunStates state);
